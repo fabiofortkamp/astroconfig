f5bfe9db33bb332c5a5bea5c919ed4dcda5b03ee
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-04-10 10:54:04 -0300

    Initial commit, migrated from v3

---
+++ README.md
@@ -0,0 +1,31 @@
+# AstroNvim Template
+
+My [AstroNvim](https://astronvim.com/) configuration, targeted mainly at Python and Go development.
+## 🛠️ Installation
+
+#### Make a backup of your current nvim and shared folder
+
+```shell
+mv ~/.config/nvim ~/.config/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+```
+
+#### Create a new user repository from this template
+
+Press the "Use this template" button above to create a new repository to store your user configuration.
+
+You can also just clone this repository directly if you do not want to track your user configuration in GitHub.
+
+#### Clone the repository
+
+```shell
+git clone https://github.com/fabiofortkamp/astroconfig ~/.config/nvim
+```
+
+#### Start Neovim
+
+```shell
+nvim
+```
---
+++ lua/community.lua
@@ -0,0 +1,70 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.julia" },
+  { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.ruby" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.clojure" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = "88",
+      disabled_filetypes = { "help", "text", "markdown" },
+      custom_colorcolumn = {},
+      scope = "file",
+    },
+  },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
+  { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.programming-language-support.nvim-jqx" },
+  { "gennaro-tedesco/nvim-jqx", ft = { "json", "yaml" } },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.editing-support.treesj" },
+  { import = "astrocommunity.debugging.nvim-bqf" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.color.tint-nvim" },
+  { "levouh/tint.nvim", opts = {
+    tint = -90,
+    saturation = 0.6,
+  } },
+  { import = "astrocommunity.color.twilight-nvim" },
+  { import = "astrocommunity.color.headlines-nvim" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.editing-support.hypersonic-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.true-zen-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  {
+    "danymat/neogen",
+    opts = {
+      languages = {
+        python = {
+          template = {
+
+            annotation_convention = "google_docstrings",
+          },
+        },
+      },
+    },
+  },
+  { import = "astrocommunity.editing-support.dial-nvim" },
+  { import = "astrocommunity.editing-support.yanky-nvim" },
+}
