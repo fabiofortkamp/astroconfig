63b2e47a521d8bdc37d58f1c81916ef37b42e454
Fábio P. Fortkamp <fabio@fabiofortkamp.com>
2024-04-15 06:46:28 -0300

    Add listoptions to show hidden characters

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,4 +1,3 @@
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
@@ -15,3 +14,18 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+
+-- Set up listchars
+-- partly stollen from:
+-- https://github.com/tjdevries/cyclist.vim
+-- https://vi.stackexchange.com/questions/31811/neovim-lua-config-how-to-append-to-listchars
+vim.opt.listchars = {
+  eol = "↵",
+  tab = "» ",
+  trail = "·",
+  extends = "<",
+  precedes = ">",
+  conceal = "┊",
+  nbsp = "␣",
+}
+vim.opt.list = true
